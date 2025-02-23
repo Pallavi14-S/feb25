99832c8e2b96639d959d35afc5613c18947e2142
Pallavi14-S <74181255+Pallavi14-S@users.noreply.github.com>
2025-02-23 23:00:14 +0530

    Assignment

---
+++ Assignment2_Day2/Snippet.java
@@ -0,0 +1,49 @@
+/*    ***** Identify Error********
+
+
+public class Main
+{
+public void main(String args[])
+{
+
+System.out.println("Hello world");
+
+}
+
+
+}
+
+
+  ERROR******* class main is public, should be declared in a
+           file named public class main  *********
+
+
+ class Main
+{
+public void main(String args[])
+{
+
+System.out.println("Hello world");
+
+}
+
+
+}
+ 
+ERROR******* Main method is not static in class Main, please define the main method as:
+   public static void main(String[] args)
+  
+
+*/
+
+ class Main
+{
+public static void main(String args[])
+{
+
+System.out.println("Hello world");
+
+}
+
+
+}
